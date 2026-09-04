62f5e16dfb383e6c7f78b56ab48a931f7539cfb0
quest82 <awoniyioluseyi@gmail.com>
2026-09-04 20:16:29 +0100

    The mess

--- 3_Exceptions/fuel/fuel.py
+++ 3_Exceptions/fuel/fuel.py
@@ -1,12 +1,28 @@
 # Create a program that requests for X (>= 0) and Y (>0) and returns X/Y as a percentage rounded to the nearest integer. 
+# If 1% or less or 99% or greater, return E or F respectively
+from math import floor
 
-def main ():
-    fraction = input("Fraction on your fuel guage: ")
-    print(get_percent(fraction))
-    
-def get_percent(value):
-    X, Y = value.split("/")
-    return f"{round((int(X)/int(Y)) * 100)}%"
+def main():
+    while True:
+        fraction = input("Fraction on your fuel guage: ")
+        X, Y = fraction.split("/")
+
+        if X.isdigit() and Y.isdigit() and int(X) <= int(Y) and int(Y) > 0:
+                fuel_percent = get_percent(X, Y)
+                print(fuel_percent)
+                break
+#     check_percent = check_percent(fuel_percent)
+
+def get_percent(a, b):
+    return floor((float(a) / float(b)) * 100) 
+
+# def check_percent(value):
+#     if value <= 1:
+#         print("E")
+#     elif value >= 99:
+#         print("F")
+#     return f"{value}%"
+
 
 
 main()
