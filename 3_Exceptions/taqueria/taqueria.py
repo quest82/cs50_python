2e18e86582a61c92a46bb2c3ce786bbaefc5c768
quest82 <awoniyioluseyi@gmail.com>
2026-09-04 21:52:27 +0100

    progress report: added base logic to collect input and calculate price

---
+++ 3_Exceptions/taqueria/taqueria.py
@@ -0,0 +1,22 @@
+felipe_taqueria = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def main():
+    total = 0
+    while True:
+        order = input("Type your order in: ")
+        total += felipe_taqueria[order.title()]
+        print(f"Total:{total}")
+
+ 
+main()
+    
