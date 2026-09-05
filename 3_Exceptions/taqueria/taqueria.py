3f91b82fccd4956a77e0f2810fddc8d2fb2afe70
quest82 <awoniyioluseyi@gmail.com>
2026-09-05 22:18:02 +0100

    progress report: Completed

--- 3_Exceptions/taqueria/taqueria.py
+++ 3_Exceptions/taqueria/taqueria.py
@@ -14,11 +14,16 @@ def main():
     total = 0
     while True:
         try:
-            order = input("Type your order in: ")
+            order = input("Item: ")
         except EOFError:
-            return
-        total += felipe_taqueria[order.title()]
-        print(f"Total:{total}")
+            print()
+            break
+        try:
+            total += felipe_taqueria[order.title()]
+        except KeyError:
+            continue
+        print(f"Total:${total:.2f}")
+        
 
  
 main()
