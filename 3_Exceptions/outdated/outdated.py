1796daa775882932b9971c5f704ae7fbc0785d84
quest82 <awoniyioluseyi@gmail.com>
2026-09-07 22:18:23 +0100

    progress report: completed logic for converting a date with written number to new format

--- 3_Exceptions/outdated/outdated.py
+++ 3_Exceptions/outdated/outdated.py
@@ -21,6 +21,7 @@ def date_converter(date):
 
     if "/" in date:
         month, day, year = date.split('/')
+
         day_no = int(day)
         month_no = int(month)
         for x in range(1, 10):
@@ -28,5 +29,30 @@ def date_converter(date):
                 day_no = f"0{day_no}"
             if month_no == x:
                 month_no = f"0{month_no}"
+
         return f"{year}-{month_no}-{day_no}"
+    else:
+        month, day, year = date.split(' ')
+        proper_day = list(day)
+        proper_day.remove(',')
+        day_no = int("".join(proper_day))
+
+        month_check = False
+
+        for index, m in enumerate(acceptable_months):
+            if month.lower() == m.lower():
+                month_no = index + 1
+                month_check = True
+        if not month_check:
+            raise ValueError
+
+        for x in range(1, 10):
+            if day_no == x:
+                day_no = f"0{day_no}"
+            if month_no == x:
+                month_no = f"0{month_no}"
+                
+        return f"{year}-{month_no}-{day_no}"
+        
+        
 main()
