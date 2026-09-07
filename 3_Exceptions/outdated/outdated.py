242e8556a32fb9baaf33ef1d81ec7faa2c4bb520
quest82 <awoniyioluseyi@gmail.com>
2026-09-07 21:44:24 +0100

    progress report: completed logic for converting a date with only numbers to new format

---
+++ 3_Exceptions/outdated/outdated.py
@@ -0,0 +1,32 @@
+def main():
+    date = input("Enter date (month/date/year: )")
+    result = date_converter(date)
+    print(result)
+
+def date_converter(date):
+    acceptable_months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+    if "/" in date:
+        month, day, year = date.split('/')
+        day_no = int(day)
+        month_no = int(month)
+        for x in range(1, 10):
+            if day_no == x:
+                day_no = f"0{day_no}"
+            if month_no == x:
+                month_no = f"0{month_no}"
+        return f"{year}-{month_no}-{day_no}"
+main()
